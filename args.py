74644673139252b6af35a70a8535e1184f91d705
Suzane <91255185+Suzanemh@users.noreply.github.com>
2024-02-12 14:55:33 +0100

    Modifie args.py

--- args.py
+++ args.py
@@ -9,7 +9,7 @@ def main():
     parser.add_argument('-p', '--port', type=int, default=8088,
                         help='port number on which the server will listen or the client will connect to '
                              '(default: %(default)s)', choices=range(1024, 65536))
-    parser.add_argument('-i', '--ip', type=str, default='127.0.0.1',
+    parser.add_argument('-i', '--ip', type=str, default='10.0.0.2',
                         help='IP address of the server\'s interface where the client should connect '
                              '(default: %(default)s, format: 10.0.0.2)')
 
@@ -20,14 +20,16 @@ def main():
         print("You cannot use both at the same time.")
         return
 
+    # Check if neither server nor client mode is specified
+    if not args.server and not args.client:
+        print("You should run either in server or client mode.")
+        return
+
     # Display port and IP addresses based on user's input
     if args.server:
-        print("Server mode enabled.")
+        print(f"The server is running with IP address = {args.ip} and port address = {args.port}")
     elif args.client:
-        print("Client mode enabled.")
-    
-    print("Port:", args.port)
-    print("IP Address:", args.ip)
+        print(f"The client is running with IP address = {args.ip} and port address = {args.port}")
 
 if __name__ == "__main__":
     main()
